eb3acf3d03495c067e989f232d3a3f27027708e4
Bulletguitarist <jyotirmoymahapatra108@gmail.com>
2026-02-21 16:52:31 +0530

    Fix netlify routing

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "baseURL: https://taskmanager-2-igpt.onrender.com/api"
 });
 
 API.interceptors.request.use(req => {
